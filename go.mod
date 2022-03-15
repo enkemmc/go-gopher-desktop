b63ac6f1a19d8f5cd8849c7cefc71375423c149b
Mark Enkema <markenkema@gmail.com>
2022-03-14 20:54:04 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/enkemmc/go-gopher-desktop
+
+go 1.17
+
+require fyne.io/fyne/v2 v2.1.3
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+	// a.SendNotification()
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+		widget.NewButton("Notify", func() {
+			not := fyne.NewNotification("a title", "great content!")
+			a.SendNotification(not)
+		}),
+	))
+
+	w.ShowAndRun()
+}
